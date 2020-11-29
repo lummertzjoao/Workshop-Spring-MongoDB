c7e6f5a56e6e8118a90d6c0d9db38e0bf4214341
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-11-29 09:50:32 -0300

    Implemented title searches with query methods

--- src/main/java/com/joaolummertz/workshopspringmongodb/repositories/PostRepository.java
+++ src/main/java/com/joaolummertz/workshopspringmongodb/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.joaolummertz.workshopspringmongodb.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.joaolummertz.workshopspringmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
